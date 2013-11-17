bc0e46ec0a13d9370540bb68267f75c3dc346fe7
Tomas Lori <tomas_lori142@hotmail.com>
2013-11-16 23:25:15 -0300

    ahora si

--- mlockfs.c
+++ mlockfs.c
@@ -11,11 +11,7 @@ static int getattr(const char *path, struct stat *stbuf) {
 	memset(stbuf, 0, sizeof(struct stat));
 	if (strcmp(path, "/") == 0) {
 		stbuf->st_mode = S_IFDIR | 0755;
-		stbuf->st_nlink = 2;
-	} else if (strcmp(path, hello_path) == 0) {
-		stbuf->st_mode = S_IFREG | 0444;
 		stbuf->st_nlink = 1;
-		stbuf->st_size = strlen(hello_str);
 	} else
 		res = -ENOENT;
 	return res;
